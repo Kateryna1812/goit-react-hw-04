8f910f98641972551b481e4b4287444f2117a404
Kateryna1812 <super.katuysha2013@gmail.com>
2024-04-07 20:51:06 +0300

    hw-04-start

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,101 @@
-import { useState, useEffect } from 'react';
-import ContactForm from './components/ContactForm/ContactForm'
-import SearchBox from './components/SearchBox/SearchBox'
-import ContactList from './components/ContactList/ContactList'
-import './App.css'
+import { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import getPhotos from "./js/API";
+//======import====components======//
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [gallery, setGallery] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  // trigger================================
+  const [trigger, setTrigger] = useState("");
+  // modal==================================
+  const [photoModal, setPhotoModal] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-   const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem('saved-contacts');
-    if (savedContacts) {
-      return JSON.parse(savedContacts);
-    }
-    return [];
-  });
-
-  const addContact = newContact => {
-    setContacts(contacts => {
-      return [...contacts, newContact];
-    });
+  const handleImageClick = (photo) => {
+    setPhotoModal(photo.urls.regular);
+    setShowModal(true);
   };
 
-  const [filter, setFilter] = useState('');
-
-  const filterContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const deleteContact = contactId => {
-    setContacts(contacts => {
-      return contacts.filter(contact => contact.id !== contactId);
-    });
+  const closeModal = () => {
+    setShowModal(false);
+    setPhotoModal("");
   };
+
+  // =========================================
 
   useEffect(() => {
-    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
-  }, [contacts]);
+    const fetchPhoto = async () => {
+      try {
+        setError(false);
+        setLoader(true);
+        if (query.trim() == "") {
+          return;
+        }
+        const photosData = await getPhotos({ query, page });
+        if (photosData.data.results.length === 0) {
+          toast.error("ми не знайшли нічого по запиту " + query);
+          setError(true);
+          return;
+        }
 
+        if (trigger === "search") {
+          setGallery(photosData.data.results);
+        } else {
+          setGallery((prevGallery) => [
+            ...prevGallery,
+            ...photosData.data.results,
+          ]);
+        }
+      } catch (error) {
+        toast.error("Oops, bad reguest");
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchPhoto();
+  }, [query, page, trigger]);
+
+  const onSearch = (searchQuery) => {
+    if (searchQuery.trim() == "") {
+      return;
+    }
+    setQuery(searchQuery);
+    setPage(1);
+    setTrigger("search");
+  };
+  const onLoadMore = () => {
+    window.scrollBy(0, 60);
+    setPage((prevPage) => prevPage + 1);
+    setTrigger("loadMore");
+  };
 
   return (
     <>
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onAddContact={addContact} />
-        <SearchBox value={filter} onSearch={setFilter} />
-        <ContactList
-          contacts={filterContacts}
-          onDeleteContact={deleteContact}
-        />
-      </div>
+      <SearchBar onSearch={onSearch} />
+      <ImageGallery gallery={gallery} handleImageClick={handleImageClick} />
+      {error && <ErrorMessage />}
+      {loader && <Loader />}
+      {gallery.length > 0 && !loader && <LoadMoreBtn onLoadMore={onLoadMore} />}
+      <ImageModal
+        isOpen={showModal}
+        closeModal={closeModal}
+        imageUrl={photoModal}
+        alt="Selected Image"
+      />
+      <Toaster position="top-right" reverseOrder={false} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
